5ed8b8ad1153d6ba2cd855d34fa21b399faf66fb
Fırat Alçın <firatalcin04@gmail.com>
2022-06-10 16:23:10 +0300

    Created Project and Layers

---
+++ BookStore/Entities/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DataStatus Status { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            Status = DataStatus.Added;
+        }
+    }
+}
---
+++ BookStore/Entities/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Model
+{
+    public class Book : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+        public int PageCount { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
